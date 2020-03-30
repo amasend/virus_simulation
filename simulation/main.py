d5460e8c21d728a556b1521a4f9e5812dee77e1a
amasend <amadeuszmasny@gmail.com>
2020-03-30 23:45:36 +0200

    - change with graph layout

--- simulation/main.py
+++ simulation/main.py
@@ -7,16 +7,6 @@ import plotly
 import plotly.graph_objs as go
 from simulation.individuals import Individual
 
-import plotly.express as px
-
-start = pd.DataFrame([[50.061, 19.923]], columns=['lat', 'lon'])
-token = open("/home/amadeusz/PycharmProjects/virus_simulation/simulation/mapbox_token").read()
-fig = px.scatter_mapbox(start, lat="lat", lon="lon",
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, autosize=True,
-                  height=900, width=900)
-
 individuals = [Individual(start_infection_probability=0.01, infection_probability=0.8) for i in range(200)]
 infected = []
 healthy = []
@@ -49,7 +39,7 @@ app.layout = html.Div(
             html.H3('Column 1'),
             dcc.Graph(id='live-graph',
                       style={
-                          'background-image': 'url(/home/amadeusz/PycharmProjects/virus_simulation/simulation/krakow_map.png)'},
+                          'background-image': 'url(https://raw.githubusercontent.com/amasend/virus_simulation/master/simulation/krakow_map.png)'},
                       animate=True,
                       figure={
                           'data': [
@@ -66,23 +56,22 @@ app.layout = html.Div(
                                'type': 'scatter',
                                'mode': 'markers',
                                'name': u'Healthy',
-                               'marker': {'color': 'blue',
+                               'marker': {'color': 'green',
                                           'size': 5}
                                },
                           ],
                           'layout': {
-                              # 'plot_bgcolor': colors['background'],
                               'plot_bgcolor': 'rgba(0,0,0,0)',
                               'paper_bgcolor': 'rgba(0,0,0,0)',
-                              # 'paper_bgcolor': colors['background'],
-
                               'font': {
                                   'color': colors['text']
                               },
                               'autosize': True,
                               'height': 900,
-                              'xaxis': dict(range=[0, 1000]),
-                              'yaxis': dict(range=[0, 1000])
+                              'xaxis': dict(range=[0, 1000], showgrid=False, zeroline=False, tickmode="array",
+                                            tickvals=[]),
+                              'yaxis': dict(range=[0, 1000], showgrid=False, zeroline=False, tickmode="array",
+                                            tickvals=[])
                           }
                       })
         ], className="six columns"),
@@ -102,7 +91,7 @@ app.layout = html.Div(
                                'y': graph_2_y_healthy,
                                'type': 'scatter',
                                'name': u'Healthy count',
-                               'marker': {'color': 'blue'}
+                               'marker': {'color': 'green'}
                                },
                           ],
                           'layout': {
@@ -117,48 +106,9 @@ app.layout = html.Div(
                               'yaxis': dict(range=[0, graph_2_y_infected[0] + graph_2_y_healthy[0]])
                           }
                       })
-        ], className="six columns"),
-        html.Div([
-            html.H3('Column 3', style={'fontColor': colors['text']}),
-            dcc.Graph(id='live-graph_3',
-                      animate=True,
-                      figure={
-                          'data': fig.data,
-                          'layout': fig.layout
-                      })
-        ], className="six columns"),
+        ], className="six columns")
     ], className='row', style={"columnCount": 2})
 )
-
-
-@app.callback(Output('live-graph_3', 'figure'), inputs=[Input('graph-update', 'n_intervals')])
-def update_graph_map(interval):
-    for individual in individuals:
-        individual.move()
-        if not individual.infected:
-            individual.check_infection(individuals=individuals)
-
-            if individual.infected:
-                infected.append(individual)
-
-    data_infected = pd.DataFrame(data={'lon': [individual.x for individual in infected],
-                                       'lat': [individual.y for individual in infected]})
-
-    data_healthy = pd.DataFrame(data={'lon': [individual.x for individual in healthy],
-                                      'lat': [individual.y for individual in healthy]})
-
-    fig_1 = px.scatter_mapbox(data_infected, lat="lat", lon="lon")
-    fig_1.update_layout(mapbox_style="open-street-map")
-
-    fig_2 = px.scatter_mapbox(data_healthy, lat="lat", lon="lon")
-    fig_2.update_layout(mapbox_style="open-street-map")
-
-    data = [
-        fig_1.data[0],
-        fig_2.data[0]
-    ]
-
-    return {'data': data}
 
 
 @app.callback(Output('live-graph', 'figure'), inputs=[Input('graph-update', 'n_intervals')])
@@ -208,8 +158,3 @@ def update_graph_2(interval):
 
 
 app.run_server(debug=True)
-
-"""
-50.097,19.822       50.097,20.033
-49.885,19.822       49.885,20.033
-"""
